b61c97b8237110087dfecc19cea466f521237d11
gures <ali_rujdi@abv.bg>
2015-05-24 19:00:30 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Znahar</title>
+        <LINK REL="stylesheet" HREF="css.css" TYPE="text/css"> 
+    </head>
+    <body>
+        <div id="header">
+            <h1 class="logo_font">ZNAHAR.BG</h1>
+            <form>
+                <input type="text" name="search" id="search">
+            </form>   
+            <div class="pos_fixed">Reklama</div>
+            <div class="pos_fixed1">Reklama</div>
+            <h2>absolue</h2>
+        </div>
+        <div id="nav">
+                <a href="">БИЛКИ</a> <a href="">БИЛКИ</a> <a href="">БИЛКИ</a> <a href="">БИЛКИ</a>
+                <a href="">БИЛКИ</a> <a href="">БИЛКИ</a> <a href="">БИЛКИ</a> <a href="">БИЛКИ</a>
+        </div>
+        <div id="conteiner">
+            <div class="news"></div>
+            <img class="img "src="bilki.jpg">
+        </div>
+        <div id="footer">
+        </div>
+    </body>
+</html>
